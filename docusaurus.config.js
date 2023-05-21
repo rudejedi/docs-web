fd93a10131af046b728bb925c6cc251bb44e93c3
rudejedi <beedowl@pm.me>
2023-05-21 18:01:04 +0530

    set editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,15 +42,13 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/rudejedi/docs-web/tree/main/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/rudejedi/docs-web/tree/main/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
