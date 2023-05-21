0a6c5ab26f0a92ff91dbc39e54df669a2f26c770
rudejedi <beedowl@pm.me>
2023-05-21 17:41:15 +0530

    reverted back and added trailingSlash:false

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,8 @@ const config = {
   url: "https://rudejedi.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/docs-web/",
+  baseUrl: "/",
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
